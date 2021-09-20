b78f090dd0acb00e22c577f27bcc548ffc2a7652
ericdavidanderson <burrej1@gmail.com>
2021-09-19 20:56:02 -0700

    weather icon duplication fixed

--- script2.js
+++ script2.js
@@ -1,41 +1,22 @@
 var cityName = document.getElementById("city");
-
-var eventEl = document.getElementById("events-container");
-var eightDay = document.getElementById("dailyForecast");
-
+var fiveDay = document.getElementById("dailyForecast");
 var responseText = document.getElementById("responseText");
 
 function displayWeather(data) {
   console.log(data);
   for (var i = 0; i < 5; i++) {
-      var modifier = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      };
-      var initDate = new Date(data[i].dt * 1000);
-      var newDate = initDate.toLocaleDateString("en-US", modifier);
-      
-    // var date = data[i].dt;
-    // var newDate = new Date(date * 1000);
+    var modifier = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    var initDate = new Date(data[i].dt * 1000);
+    var newDate = initDate.toLocaleDateString("en-US", modifier);
     iconCode = data[i].weather[0].icon;
-    currentIcon = data[0].weather[0].icon;
-    eightDay = data[i].weather[0].main;
-    morningTemp = Math.round(data[i].temp.morn);
-    eveTemp = Math.round(data[i].temp.eve);
 
-
-
-
-    // var cityNow = document.createElement('p');
-    // document.getElementById("currentCity").appendChild(cityNow);
-    // cityNow.setAttribute("class", "cityNow");
-    // currentDate = new Date(date * 1000);
-    // cityNow.textContent = cityName + currentDate;
+    fiveDay = data[i].weather[0].main;
     
-
-    // currentDate = new Date(date * 1000);
 
     var weatherContainer = document.createElement("p");
     document.getElementById("fiveDay").appendChild(weatherContainer);
@@ -47,31 +28,16 @@ function displayWeather(data) {
     wCard.setAttribute("class", "date");
 
     var weatherIcon = document.createElement("img");
-    var todayIcon = document.createElement("img")
-    currentIcon === data[0].weather[0].icon;
     weatherIcon.src = "http://openweathermap.org/img/wn/" + iconCode + ".png";
-    todayIcon.src = "http://openweathermap.org/img/wn/" + currentIcon + ".png";
     wCard.appendChild(weatherIcon);
-    currentCity.appendChild(todayIcon);
     weatherIcon.setAttribute("class", "wImage");
-    todayIcon.setAttribute('class', 'currentImage');
 
     var conditions = document.createElement("div");
-    conditions.textContent = eightDay;
+    conditions.textContent = fiveDay;
     wCard.appendChild(conditions);
     conditions.setAttribute("class", "wConditions");
 
-    // var amTemp = document.createElement("div");
-    // amTemp.textContent = "Morning Temperature: " + morningTemp;
-    // conditions.appendChild(amTemp);
-    // amTemp.setAttribute("class", "earlyTemp");
-
-    // var pmTemp = document.createElement("div");
-    // pmTemp.textContent = "Evening Temperature: " + eveTemp;
-    // amTemp.appendChild(pmTemp);
-    // pmTemp.setAttribute("class", "eveTemp");
-
-    console.log(newDate, weatherIcon, eightDay, morningTemp, eveTemp);
+    // console.log(newDate, weatherIcon, fiveDay, morningTemp, eveTemp);
   }
 }
 
@@ -91,64 +57,63 @@ function getWeather() {
       console.log(response.name);
       gps(response.coord.lat, response.coord.lon);
 
-      var currentCity = document.createElement('div')
+      var currentCity = document.createElement("div");
       currentCity.setAttribute("class", "cityCard");
-       document.getElementById("currentCity").appendChild(currentCity);
+      document.getElementById("currentCity").appendChild(currentCity);
 
-       var modifier = {
-         weekday: "long",
-         year: "numeric",
-         month: "long",
-         day: "numeric",
-       };
-        var initDate = new Date(response.dt * 1000);
-       var date =  initDate.toLocaleDateString('en-US', modifier);
       
+      var modifier = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
+      var initDate = new Date(response.dt * 1000);
+      var date = initDate.toLocaleDateString("en-US", modifier);
 
-       city = response.name;
-       temp = response.main.temp;
-       wind = response.wind.speed;
-       humid = response.main.humidity;
+      city = response.name;
+      temp = response.main.temp;
+      wind = response.wind.speed;
+      humid = response.main.humidity;
 
-       console.log(city, temp, wind, humid)
+      console.log(city, temp, wind, humid);
+
+       var cityDate = document.createElement("li");
+       cityDate.innerText =
+         " Welcome to " + city + " weather. Today is; " + date;
+       currentCity.appendChild(cityDate);
 
       var ul = document.createElement("ul");
-        ul.setAttribute("style", "padding:5; margin:5;");
-        ul.setAttribute("id", "currentList");
-       currentCity.appendChild(ul);
-       
-       
-       
-      
-       var cityDate = document.createElement('li');
-           cityDate.innerText = " Welcome to "  + city +" weather. Today is; " +date;
-           ul.appendChild(cityDate);
+      ul.setAttribute("style", "padding:5; margin:5;");
+      ul.setAttribute("id", "currentList");
+      currentCity.appendChild(ul);
+
+      currentIcon = response.weather[0].icon;
+      var todayIcon = document.createElement("img");
+      todayIcon.src =
+        "http://openweathermap.org/img/wn/" + currentIcon + ".png";
+      ul.appendChild(todayIcon);
+      todayIcon.setAttribute("class", "currentImage");
 
 
-        var currTemp = document.createElement('li');
-        currTemp.innerText = "Temperature: "+ temp;
-        ul.appendChild(currTemp);
+     
 
-        var breeze = document.createElement('li');
-        breeze.innerText = "Wind: "+ wind
-        ul.appendChild(breeze);
-    
-        var humidity = document.createElement('li');
-        humidity.innerText = "Humidity: " + humid;
-        ul.appendChild(humidity);
+      var currTemp = document.createElement("li");
+      currTemp.innerText = "Temperature: " + temp;
+      ul.appendChild(currTemp);
 
-        
+      var breeze = document.createElement("li");
+      breeze.innerText = "Wind: " + wind;
+      ul.appendChild(breeze);
 
-         
-       
-    
-       console.log(cityDate, currTemp, breeze, humidity)
-    //    li.setAttribute('style', 'display:block;');
-    //    ul.appendChild(li);
-    //    currentCity.appendChild(ul);
-       
+      var humidity = document.createElement("li");
+      humidity.innerText = "Humidity: " + humid;
+      ul.appendChild(humidity);
 
-       
+      console.log(cityDate, currTemp, breeze, humidity);
+      //    li.setAttribute('style', 'display:block;');
+      //    ul.appendChild(li);
+      //    currentCity.appendChild(ul);
     });
 }
 function gps(lat, long) {
