f49cab8ba6815720c0b3fff68f27772d207f406f
ericdavidanderson <burrej1@gmail.com>
2021-10-06 01:00:56 -0700

    local storage, and color coded uv index

--- script2.js
+++ script2.js
@@ -1,10 +1,32 @@
 var cityName = document.getElementById("city");
 var fiveDay = document.getElementById("dailyForecast");
 var responseText = document.getElementById("responseText");
+ var historyCity =document.getElementById("pastCity");
+var cityTrack = JSON.parse(window.localStorage.getItem("pastCity"))||[]
+ 
+ 
 
 function displayWeather(data) {
+  document.getElementById("fiveDay").innerHTML = "";
+  uv = data[0].uvi;
+   var uvindex = document.createElement("ul");
+   if (uv<3){
+     uvindex.setAttribute("class", "uvblue");
+     console.log("under3");
+    }
+    else if (uv>7){
+      uvindex.setAttribute("class", "uvred");
+     console.log ("high");}
+     else {
+       uvindex.setAttribute('class', "uvyellow");
+     console.log("medium");}
+   
+   uvindex.textContent = "UV index: " + uv;
+   currentCity.appendChild(uvindex);
+   uvindex.setAttribute("id", "uv");
+
   console.log(data);
-  for (var i = 0; i < 5; i++) {
+  for (var i = 1; i < 6; i++) {
     var modifier = {
       weekday: "long",
       year: "numeric",
@@ -14,8 +36,12 @@ function displayWeather(data) {
     var initDate = new Date(data[i].dt * 1000);
     var newDate = initDate.toLocaleDateString("en-US", modifier);
     iconCode = data[i].weather[0].icon;
-
     fiveDay = data[i].weather[0].main;
+    temp = data[i].temp.day;
+    windspeed = data[i].wind_speed;
+    gusts = data[i].wind_gust;
+    humidity = data[i].humidity;
+    
     
 
     var weatherContainer = document.createElement("p");
@@ -27,24 +53,41 @@ function displayWeather(data) {
     weatherContainer.appendChild(wCard);
     wCard.setAttribute("class", "date");
 
-    var weatherIcon = document.createElement("img");
-    weatherIcon.src = "http://openweathermap.org/img/wn/" + iconCode + ".png";
-    wCard.appendChild(weatherIcon);
-    weatherIcon.setAttribute("class", "wImage");
-
+    
     var conditions = document.createElement("div");
     conditions.textContent = fiveDay;
     wCard.appendChild(conditions);
     conditions.setAttribute("class", "wConditions");
 
-    // console.log(newDate, weatherIcon, fiveDay, morningTemp, eveTemp);
+    var weatherIcon = document.createElement("img");
+    weatherIcon.src = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+    conditions.appendChild(weatherIcon);
+    weatherIcon.setAttribute("class", "wImage");
+
+    
+    var temperature = document.createElement("div");
+    temperature.textContent = "Temperature: " +temp;
+    wCard.appendChild(temperature);
+
+    var windSpeed = document.createElement("div");
+    windSpeed.textContent = windspeed + " (with gusts as high as "+ gusts +")."
+    wCard.appendChild(windSpeed);
+    
+    var humid = document.createElement("div");
+    humid.textContent = "Humidity: " + humidity+"%";
+    wCard.appendChild(humid);
+
+   
+  
+
   }
 }
 
-function getWeather() {
+function getWeather(city) {
+  
   var apiRequest =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
-    cityName.value +
+    city +
     "&appid=15a2f6e975005b96c0df56340849949d&units=imperial";
   console.log(apiRequest);
   fetch(apiRequest)
@@ -59,6 +102,7 @@ function getWeather() {
 
       var currentCity = document.createElement("div");
       currentCity.setAttribute("class", "cityCard");
+      document.getElementById("currentCity").innerHTML = "";
       document.getElementById("currentCity").appendChild(currentCity);
 
       
@@ -75,12 +119,13 @@ function getWeather() {
       temp = response.main.temp;
       wind = response.wind.speed;
       humid = response.main.humidity;
+      
 
       console.log(city, temp, wind, humid);
 
        var cityDate = document.createElement("li");
        cityDate.innerText =
-         " Welcome to " + city + " weather. Today is; " + date;
+         " Welcome to " + city + " weather. Today is: " + date;
        currentCity.appendChild(cityDate);
 
       var ul = document.createElement("ul");
@@ -111,9 +156,6 @@ function getWeather() {
       ul.appendChild(humidity);
 
       console.log(cityDate, currTemp, breeze, humidity);
-      //    li.setAttribute('style', 'display:block;');
-      //    ul.appendChild(li);
-      //    currentCity.appendChild(ul);
     });
 }
 function gps(lat, long) {
@@ -132,4 +174,34 @@ function gps(lat, long) {
       displayWeather(response.daily);
     });
 }
-document.getElementById("submitCity").addEventListener("click", getWeather);
+document.getElementById("submitCity").addEventListener("click", function(){
+  getWeather(cityName.value)
+});
+document.getElementById('submitCity').addEventListener('click', storeCity);
+
+function storeCity() {
+  var city = cityName.value.trim();
+  console.log(city)
+  cityTrack.push(city);
+  window.localStorage.setItem("pastCity", JSON.stringify(cityTrack));
+  
+historyList();
+}
+function historyList(){
+  historyCity.innerHTML="";
+  for (let i = 0; i < cityTrack.length; i++) {
+   var li = document.createElement("li");
+   var btn = document.createElement("button");
+   btn.setAttribute("class", "historyButton");
+   btn.textContent=cityTrack[i];
+   btn.onclick=historySearch;
+   li.append(btn);
+   historyCity.append(li);
+    
+  }
+} function historySearch(){
+  console.log(this)
+  console.log(this.textContent)
+  getWeather(this.textContent)
+}
+historyList();
